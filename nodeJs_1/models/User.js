35ebc74fcf1a3a445079a488473ab58fd8166d73
Jubayar Ahamad <oscyanogen@gmail.com>
2024-01-22 20:25:51 +0600

    Commit-10 : apply clean code

--- nodeJs_1/models/User.js
+++ nodeJs_1/models/User.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    id: {
+        type: Number
+    },
     fname: {
         type: String
     },
--- nodeJs_1/routes/api/users.js
+++ nodeJs_1/routes/api/users.js
@@ -5,14 +5,18 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const authenticateToken = require('../../middleware/auth');
 
+
+let lastId = 0
 // Create a user
 router.post('/', async(req, res) => {
     console.log('Request Body:', req.body);
     try {
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(req.body.password, salt);
-
+        
+        lastId = ++lastId
         const userObject = {
+        id: lastId,
         fname: req.body.fName,
         lname: req.body.lName,
         email: req.body.email,
@@ -33,74 +37,12 @@ router.post('/', async(req, res) => {
 router.post('/login', async(req, res) => {
     try {
         const {type, email, password, refreshToken} = req.body; //check
-        if(type== 'email') {
+        if(type == 'email') {
             //Step 1: Find the user by email
-            const user = await User.findOne({email: email});
-            if(!user) {
-                res.status(401).json({massage: "User not found"})
-            }
-            else {
-                // Step 2: If user found match password
-                const isValidPassword = await bcrypt.compare(password, user.password);  //check
-                if(!isValidPassword) {
-                    res.status(401).json({massage: "Password is wrong"});
-                }
-                else {
-                    // Step 3: If password match then token generate
-                    const accessToken = jwt.sign(
-                       { email: user.email, id: user._id },  //check
-                       process.env.JWT_SECRET,    //check
-                       { expiresIn: "7d" }
-                    );
-
-                    const refreshToken = jwt.sign(
-                        { email: user.email, id: user._id },  //check
-                        process.env.JWT_SECRET,
-                        { expiresIn: "30d" }
-                     );
-                    
-                     const userObj = user.toJSON();
-                     userObj['accessToken'] = accessToken;  //check
-                     userObj['refreshToken'] = refreshToken;
-                     res.status(200).json(userObj);
-                }
-            }
+            await emailLogin(email, res, password);
         }
         else {
-            if(!refreshToken) {
-                res.status(401).json({massage: 'Refresh token is not defined'});
-            }else {
-                jwt.verify(refreshToken, process.env.JWT_SECRET, async(err, payload) => {  //check
-                    if(err) {
-                        res.status(401).json({massage: "Unauthorize"});
-                        return;
-                    }else {
-                        const id = payload.id;
-                        const user = await User.findById(id);
-                        if(!user) {
-                            res.status(401).json({massage: "User not found"})
-                        }else {
-                            const accessToken = jwt.sign(
-                                { email: user.email, id: user._id },  //check
-                                process.env.JWT_SECRET,    //check
-                                { expiresIn: "7d" }
-                             );
-         
-                             const refreshToken = jwt.sign(
-                                 { email: user.email, id: user._id },  //check
-                                 process.env.JWT_SECRET,
-                                 { expiresIn: "30d" }
-                              );
-                             
-                              const userObj = user.toJSON();
-                              userObj['accessToken'] = accessToken;  //check
-                              userObj['refreshToken'] = refreshToken;
-                              res.status(200).json(userObj);
-                        }
-                        
-                    }
-                })
-            }
+            refreshTokenLogin(refreshToken, res);
         }
     }
     catch (error) {
@@ -208,3 +150,64 @@ router.delete('/', async (req, res) => {
 
 
 module.exports = router;
+
+
+
+function refreshTokenLogin(refreshToken, res) {
+    if (!refreshToken) {
+        res.status(401).json({ massage: 'Refresh token is not defined' });
+    } else {
+        jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
+            if (err) {
+                res.status(401).json({ massage: "Unauthorize" });
+                return;
+            } else {
+                const id = payload.id;
+                const user = await User.findById(id);
+                if (!user) {
+                    res.status(401).json({ massage: "User not found" });
+                } else {
+                    getUserToken(user, res);
+                }
+
+            }
+        });
+    }
+}
+
+async function refreshTokenLogin(email, res, password) {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        res.status(401).json({ massage: "User not found" });
+    }
+    else {
+        // Step 2: If user found match password
+        const isValidPassword = await bcrypt.compare(password, user.password); //check
+        if (!isValidPassword) {
+            res.status(401).json({ massage: "Password is wrong" });
+        }
+        else {
+            // Step 3: If password match then token generate
+            getUserToken(user, res);
+        }
+    }
+}
+
+function getUserToken(user, res) {
+    const accessToken = jwt.sign(
+        { email: user.email, id: user._id }, //check
+        process.env.JWT_SECRET, //check
+        { expiresIn: "7d" }
+    );
+
+    const refreshToken = jwt.sign(
+        { email: user.email, id: user._id }, //check
+        process.env.JWT_SECRET,
+        { expiresIn: "30d" }
+    );
+
+    const userObj = user.toJSON();
+    userObj['accessToken'] = accessToken; //check
+    userObj['refreshToken'] = refreshToken;
+    res.status(200).json(userObj);
+}
